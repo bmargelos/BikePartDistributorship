58f5c10b05a624a46be96e72b7987c367a8fbcb8
bmargelos <57237131+bmargelos@users.noreply.github.com>
2020-02-01 01:44:40 -0500

    Lab 2

---
+++ src/BikePart.java
@@ -0,0 +1,128 @@
+//creating a BikePart class
+
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * This class stores information about a bike part
+ * @author Brittany Margelos
+ *
+ */
+
+class BikePart {
+
+   public String partName;
+   public String partNumber;
+   public double price, salesPrice;
+   public boolean onSale;
+
+    /**
+     * This constructs a bike part that has a specified a part name, a part number,
+     * the price, the sale price, and if the bike part is on sale.
+     * A string array called "temp" that when the info is passed through, it will split at the ",".
+     * Each current object of the info constructor will be saved into a specific index of temp array
+     * @param info stores all the biker part info
+     *
+     */
+
+    public BikePart(String info) {
+        String[] temp = info.split(",");
+        this.partName = temp[0];
+        this.partNumber = temp[1];
+        this.price = Double.parseDouble(temp[2]);
+        this.salesPrice = Double.parseDouble(temp[3]);
+        if (onSale = temp[4].equals("true")){
+            onSale=true;
+        }
+        else{
+            onSale=false;
+        }
+
+    }//end of BikePart constructor
+
+    /**
+     *To set a part name to bike part
+     * @param partName a new bike part name
+     */
+    public void setName(String partName){
+        this.partName=partName;
+    }//end of setting partName
+
+    /**
+     * To set a part number to bike part
+     * @param partNumber a new bike part number
+     */
+    public void setPartNum(String partNumber){
+
+        this.partNumber=partNumber;
+    }//end of setting partNumber
+
+    /**
+     * To set a price to bike part
+     * @param price a new bike part price
+     */
+    public void setPrice(double price){
+
+        this.price=price;
+    }//end of setting price
+
+    /**
+     * To set a sales price to bike part
+     * @param salesPrice a new bike part sales price
+     */
+    public void setPricesSales(double salesPrice){
+
+        this.salesPrice=salesPrice;
+    }//end of setting salesPrice
+
+    /**
+     * To set if bike part is on sale
+     * @param onSale a new bike part that is on sale
+     */
+    public void setOnSale(boolean onSale){
+        this.onSale=onSale;
+    }//end of onSale
+
+
+    /**
+     *get bike part name
+     * @return part name
+     */
+    public String getName(){
+        return this.partName;
+    }//end of GetName
+
+    /**
+     *get bike part number
+     * @return part number
+     */
+    public String getPartNumber(){
+        return this.partNumber;
+    }//end of GetPartNumber
+
+    /**
+     *get bike part price
+     * @return price
+     */
+    public double getPrice(){
+        return this.price;
+    }//end of GetPrice
+
+    /**
+     *get bike part sale price
+     * @return sale price
+     */
+    public double getSalesPrice(){
+        return this.salesPrice;
+    }//end of GetPrice
+
+    /**
+     *get bike part on sale
+     * @returns true or false depending if bike part is on the sale
+     */
+    public boolean getOnSale() {
+        return this.onSale;
+    }// end of getOnSale
+
+
+}//end of BikePart class
---
+++ src/Main.java
@@ -0,0 +1,40 @@
+//I Pledge
+//Brittany Margelos
+
+import java.util.Scanner;
+public class Main {
+    /**
+     *
+     * @param args
+     *prints how many lines the user enters
+     * printing all parts that cost less than $20
+     */
+    public static void main(String[] args){
+        // creating a scanner so a user can enter how many lines they want to put in the program
+        Scanner in = new Scanner(System.in);
+        System.out.print("How many lines do you have?");
+        int num = in.nextInt();
+        System.out.println(num);
+        //user can enter bike part info
+        System.out.println("Now enter the lines:");
+
+
+        // creating a 4-loop that goes up to the number of lines the user has entered
+        BikePart [] bpArray= new BikePart[num];
+        for( int x = 0; x<num; x++){
+            String info = in.next();
+            BikePart bp = new BikePart(info);
+            bpArray[x]=bp;
+            System.out.println(info);
+        }//end of 4-loop
+
+        //making a 4-loop and printing all parts that cost less than $20
+        System.out.println("");
+        System.out.println("Now printing the bike parts that are less than $20");
+        for (int x=0; x<bpArray.length; x++){
+            if(bpArray[x].price<20)
+                System.out.println(bpArray[x].partName+ "," + bpArray[x].partNumber + ","+ bpArray[x].price+ "," + bpArray[x].salesPrice + "," + bpArray[x].onSale);
+        }// end of 4-loop
+
+    }//end of PSVM
+}//end of Main class
