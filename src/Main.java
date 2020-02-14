a803905cb8aecbbe1c42bdb2d48c7dee440158b2
bmargelos <57237131+bmargelos@users.noreply.github.com>
2020-02-14 15:25:05 -0500

    read file

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,23 @@
 //I Pledge
 //Brittany Margelos
-//BRO
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 
-import java.io.InputStreamReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Main {
     /**
-     *
-     * @param args
-     *prints how many lines the user enters
-     * printing all parts that cost less than $20
+     * @param args prints how many lines the user enters
+     *             printing all parts that cost less than $20
      */
     public static void main(String[] args) throws FileNotFoundException {
 
-
-
-
-
-    }
-        public String FileReader() throws FileNotFoundException {
-            BufferedReader reader = new BufferedReader(new FileReader("testinventory.txt"))
-            public String readAllLines (BufferedReader reader) throws IOException {
-                StringBuilder content = new StringBuilder();
-                String line;
-
-                while ((line = reader.readLine()) != null) {
-                    content.append(line);
-                    content.append(System.lineSeparator());
-                }
-
-                return content.toString();
-            }
+        File inFile = new File("input.txt");
+        Scanner sc = new Scanner(inFile);
+        while (sc.hasNextLine()) ;
+        {
+            String line = sc.nextLine();
+            System.out.println(line);
         }
-
-
-        /*        // creating a scanner so a user can enter how many lines they want to put in the program
-        Scanner in = new Scanner(System.in);
-        System.out.print("How many lines do you have?");
-        int num = in.nextInt();
-        System.out.println(num);
-        //user can enter bike part info
-        System.out.println("Now enter the lines:");
-*/
-
-
-
-
-
+        sc.close();
     }//end of PSVM
-}//end of Main class
+}//end of main class
