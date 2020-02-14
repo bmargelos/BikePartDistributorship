d122cac83a9cd48481aa9a76bce240a4881a8764
Louiebandz <60198881+Louiebandz@users.noreply.github.com>
2020-02-14 16:49:59 -0500

    Choices

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,14 @@ public class Main {
     
     public static void main(String[] args) throws FileNotFoundException{
     FileReader("warehouseDB.txt");
+    Scanner Input = new Scanner(System.in);
+    System.out.println("Enter a choice");
+    String UserChoice = Input.next();
+    String Choice = "";
+    while(Choice != "Quit"){
+        System.out.println("Please select an option:"+"Read: Read an inventory delivery file"+"Enter: Enter a part"+"Sell: Sell a part" +"Display: display a part"+"SortName: Sort and Display parts by name"+"SortNumber: Sort parts by part name");in
+        
+    }
 
     }
 
@@ -21,7 +29,7 @@ public class Main {
             Scanner sc = new Scanner(inFile);
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
-                
+
             }
             sc.close();
         }
