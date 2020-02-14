f95cdedd97a87442e8e8ea2adefd14430d83b63e
bmargelos <57237131+bmargelos@users.noreply.github.com>
2020-02-14 15:59:15 -0500

    read file

--- src/Main.java
+++ src/Main.java
@@ -9,15 +9,14 @@ public class Main {
      * @param args prints how many lines the user enters
      *             printing all parts that cost less than $20
      */
-    public static void main(String[] args) throws FileNotFoundException {
 
-        File inFile = new File("input.txt");
-        Scanner sc = new Scanner(inFile);
-        while (sc.hasNextLine()) ;
-        {
-            String line = sc.nextLine();
-            System.out.println(line);
-        }
-        sc.close();
-    }//end of PSVM
+ public static void main(String[] args) throws FileNotFoundException {
+     File inFile = new File("warehouseDB.txt");
+     Scanner sc = new Scanner(inFile);
+     while (sc.hasNextLine()) {
+         String line = sc.nextLine();
+         System.out.println(line);
+     }
+     sc.close();
+ }//end of PSVM
 }//end of main class
