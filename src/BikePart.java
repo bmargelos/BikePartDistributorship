26bccf9750f98967005978013f5934074dd04153
Louiebandz <60198881+Louiebandz@users.noreply.github.com>
2020-02-14 15:32:53 -0500

    Fixed bike part class g

--- src/BikePart.java
+++ src/BikePart.java
@@ -9,18 +9,18 @@
 //color
 class BikePart {
     public String partName;
-   public int partNumber;
-   public double price, salesPrice;
-   public boolean onSale;
-   public int quantity;
+    public int partNumber;
+    public double price, salesPrice;
+    public boolean onSale;
+    public int quantity;
 
     /**
      * This constructs a bike part that has a specified a part name, a part number,
      * the price, the sale price, whether the bike part is on sale, and it's quantity.
      * A string array called "temp" that when the info is passed through, it will split at the ",".
      * Each current object of the info constructor will be saved into a specific index of temp array
-     * @param info stores all the biker part info
      *
+     * @param info stores all the biker part info
      */
 
     public BikePart(String info) {
@@ -29,98 +29,116 @@ class BikePart {
         this.partNumber = Integer.parseInt(temp[1]);
         this.price = Double.parseDouble(temp[2]);
         this.salesPrice = Double.parseDouble(temp[3]);
-        if (onSale = temp[4].equals("true")){
-            onSale=true;
-        }
-        else{
-            onSale=false;
+        if (onSale = temp[4].equals("true")) {
+            onSale = true;
+        } else {
+            onSale = false;
         }
         this.quantity = Integer.parseInt(temp[5]);
     }//end of BikePart constructor
 
     /**
-     *To set a part name to bike part
+     * To set a part name to bike part
+     *
      * @param partName a new bike part name
      */
-    public void setName(String partName){
-        this.partName=partName;
+    public void setName(String partName) {
+        this.partName = partName;
     }//end of setting partName
 
     /**
      * To set a part number to bike part
+     *
      * @param partNumber a new bike part number
      */
-    public void setPartNumber(int partNumber){
+    public void setPartNumber(int partNumber) {
 
         this.partNumber = partNumber;
     }//end of setting partNumber
 
     /**
      * To set a price to bike part
+     *
      * @param price a new bike part price
      */
-    public void setPrice(double price){
+    public void setPrice(double price) {
 
-        this.price=price;
+        this.price = price;
     }//end of setting price
 
     /**
      * To set a sales price to bike part
+     *
      * @param salesPrice a new bike part sales price
      */
-    public void setPricesSales(double salesPrice){
+    public void setPricesSales(double salesPrice) {
 
-        this.salesPrice=salesPrice;
+        this.salesPrice = salesPrice;
     }//end of setting salesPrice
 
     /**
      * To set if bike part is on sale
+     *
      * @param onSale a new bike part that is on sale
      */
-    public void setOnSale(boolean onSale){
-        this.onSale=onSale;
+    public void setOnSale(boolean onSale) {
+        this.onSale = onSale;
     }//end of onSale
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
 
     /**
-     *get bike part name
+     * get bike part name
+     *
      * @return part name
      */
-    public String getName(){
+    public String getName() {
         return this.partName;
     }//end of GetName
 
     /**
-     *get bike part number
+     * get bike part number
+     *
      * @return part number
      */
-    public int getPartNumber(){
+    public int getPartNumber() {
         return this.partNumber;
     }//end of GetPartNumber
 
     /**
-     *get bike part price
+     * get bike part price
+     *
      * @return price
      */
-    public double getPrice(){
+    public double getPrice() {
         return this.price;
     }//end of GetPrice
 
     /**
-     *get bike part sale price
+     * get bike part sale price
+     *
      * @return sale price
      */
-    public double getSalesPrice(){
+    public double getSalesPrice() {
         return this.salesPrice;
     }//end of GetPrice
 
     /**
-     *get bike part on sale
+     * get bike part on sale
+     *
      * @return true or false depending if bike part is on the sale
      */
     public boolean getOnSale() {
         return this.onSale;
+
     }// end of getOnSale
+
+    public int getQuantity(){
+        return this.quantity;
+    }
 
 
 }//end of BikePart class
