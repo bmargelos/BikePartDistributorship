e43425c3d4f53b9cf7dea058e62900b44f335357
Louiebandz <60198881+Louiebandz@users.noreply.github.com>
2020-02-14 15:59:42 -0500

    File read test

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileInputStream;
 import java.util.Scanner;
 public class Main {
-    /**
-     * @param args prints how many lines the user enters
-     *             printing all parts that cost less than $20
-     */
+    
     public static void main(String[] args) throws FileNotFoundException {
 
 
@@ -18,7 +15,7 @@ public class Main {
         FileInputStream inFile;
         inFile = new FileInputStream("inventory2.txt");
         Scanner sc = new Scanner(inFile);
-        while (sc.hasNextLine()) ;
+        while (sc.hasNextLine())
         {
             String line = sc.nextLine();
             System.out.println(line);
