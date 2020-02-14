03a8a842e2b936459d8b2a29938954aa8a34ccce
Louiebandz <60198881+Louiebandz@users.noreply.github.com>
2020-02-14 17:47:54 -0500

    Choices

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,40 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         FileReader("warehouseDB.txt");
+        Choices();
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static void Choices(){
         Scanner Input = new Scanner(System.in);
-
-
         String Choice = "";
-
         while (!Choice.equalsIgnoreCase("Quit")) {
-                System.out.println("Please select an option: \n" + "Read: Read an inventory delivery file \n" + "Enter: Enter a part \n" + "Sell: Sell a part \n" + "Display: display a part \n" + "SortName: Sort and Display parts by name \n" + "SortNumber: Sort parts by part name ");
-                System.out.println("Enter a choice:");
-                Choice = Input.next();
-
-
-            }
+            System.out.println("Please select an option: \n" + "Read: Read an inventory delivery file \n" + "Enter: Enter a part \n" + "Sell: Sell a part \n" + "Display: display a part \n" + "SortName: Sort and Display parts by name \n" + "SortNumber: Sort parts by part name ");
+            System.out.println("Enter a choice:");
+            Choice = Input.next();
 
         }
+    }
+
 
 
         public static void FileReader (String filename) throws FileNotFoundException {
